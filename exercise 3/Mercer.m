41921c056edc591ff6c6a8a8410b1bd7983bafde
Konstantinos Lagaros <126508703+Junkyykl@users.noreply.github.com>
2024-05-20 15:06:49 +0300

    Delete exercise 3 directory

--- exercise 3/Mercer.m
+++
@@ -1,3 +0,0 @@
-function y = Mercer(x,y,h)
-    y = exp(-1/h * ( (x(1)-y(1))^2 + (x(2)-y(2))^2 ) );
-end
--- exercise 3/pdf_estim.m
+++
@@ -1,9 +0,0 @@
-function y = pdf_estim(x,Y,N,h)
-    y = zeros(1,length(x));
-    for i = 1:length(x)
-        for j = 1:N
-            y(i) = y(i) + Kernel(x(i)-Y(j),h);            
-        end
-        y(i) = y(i)/N;
-    end
-end
